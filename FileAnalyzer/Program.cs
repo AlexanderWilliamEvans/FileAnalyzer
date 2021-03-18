12e08abd547e06ab86c12ea6b99e41d20fe5d346
SOKIGO\alev <alexander.evans@sokigo.com>
2021-03-18 15:50:03 +0100

    first commit

---
+++ FileAnalyzer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ FileAnalyzer/Reader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileAnalyzer
+{
+    public class Reader
+    {
+        public string dir = "";
+
+        public Reader(string dir)
+        {
+            this.dir = dir;
+        }
+
+        public void DirectorySearch(string dir)
+        {
+            try
+            {
+                foreach (string f in Directory.GetFiles(dir))
+                {
+                    Console.WriteLine(Path.GetFileName(f));
+                }
+                foreach (string d in Directory.GetDirectories(dir))
+                {
+                    Console.WriteLine(Path.GetFileName(d));
+                    DirectorySearch(d);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void GetDirectories()
+        {
+            List<string> AllFiles = new List<string>();
+            void ParsePath(string path)
+            {
+                string[] SubDirs = Directory.GetDirectories(path);
+                AllFiles.AddRange(SubDirs);
+                AllFiles.AddRange(Directory.GetFiles(path));
+                foreach (string subdir in SubDirs)
+                    ParsePath(subdir);
+            }
+        } 
+    }
+};
